21f8d7178e2dd1452ffffdca47513f280603bcee
G51DHU <gs_sidhu@hotmail.co.uk>
2022-07-02 15:20:31 +0100

    Removed unecessary packages.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import preact from "@preact/preset-vite";
-import { terser } from "rollup-plugin-terser";
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
-    preact({ devtoolsInProd: true }), 
-    terser(),
-    visualizer()
-  ],
-  resolve:{
-    alias:{
-    "@c": __dirname + "/src/components",
-    "@a": __dirname + "/src/assets",
-    "@s": __dirname + "/src/styles",
-    }
-  }
+    preact({ devtoolsInProd: true })
+  ]
 })
